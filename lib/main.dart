50a403579ff9a226a1c8ceb524cef7f736a5681e
ahamd <abo.7fl@gmail.com>
2023-03-01 20:26:52 +0300

    update title2

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Journaly"),
+          title: Text("stay Healthy"),
           centerTitle: true,
           backgroundColor: Colors.green,
         ),
