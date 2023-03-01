91fb734c9ce130aa5d286447124a1507700adad0
unknown <>
2023-03-01 19:11:10 +0300

    flter an log in  app

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,55 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello World!'),
+              Text('Welcome back'),
+              Container(
+                height: 30,
+                width: 100,
+                child: Center(child: Text('Username')),
+                color: Colors.white,
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 30,
+                width: 100,
+                child: Center(child: Text('Password')),
+                color: Colors.white,
+              ),
+              Text('Reset password'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('----'),
+                  Text('Continue with'),
+                  Text('----'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Google'),
+                  Text('Apple'),
+                  Text('FaceBook'),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('do you have an account ?'),
+                  Text('Create new account'),
+                ],
+              )
+            ],
+          ),
         ),
+        backgroundColor: Color.fromARGB(255, 197, 251, 200),
       ),
     );
   }
