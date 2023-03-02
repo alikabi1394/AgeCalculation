be63904b123cab1aeeec57023ccb5b3758102d15
unknown <>
2023-03-02 20:09:07 +0300

    chng user in pasoord

--- lib/Login.dart
+++ lib/Login.dart
@@ -14,14 +14,14 @@ class Login extends StatelessWidget {
         Container(
           height: 30,
           width: 100,
-          child: Center(child: Text('Username')),
+          child: Center(child: Text('اسم المستخدم')),
           color: Colors.white,
         ),
         Container(
           margin: EdgeInsets.all(10),
           height: 30,
           width: 100,
-          child: Center(child: Text('Password')),
+          child: Center(child: Text(' كلمة المرور')),
           color: Colors.white,
         ),
         Text('Reset password'),
